20d6723f1be557a873c268b3b44be6203af75ed1
Casano Kirlew <150434408+Cas64288@users.noreply.github.com>
2026-04-26 17:07:56 +0100

    Update parameters and comments in Kitaev model code

--- U(t)_driven_Kitaev_model_FT.py
+++ U(t)_driven_Kitaev_model_FT.py
@@ -1,7 +1,13 @@
+# Compute Floquet quasienergy bands for the periodically driven Kitaev honeycomb model in the anomalous phase
+# Fourier transform in k_y-> y while keeping k_x as a good quantum number 
+# Code also contains a pertubation test to see if energies are still prtected for small pertubations of the drive
+
 import numpy as np
 from scipy.linalg import expm
 import matplotlib.pyplot as plt
 
+
+# The Hamiltonian is first written in mixed momentum space then Fourier transformed over ky to produce a real-space description along y.
 def kitaev_ft_Hamiltonian_FT_OBC(kx, Jx, Jy, Jz, a0, N):
     sig_x = np.array([[0, 1], [1, 0]], dtype=complex)
     sig_y = np.array([[0, -1j], [1j, 0]], dtype=complex)
@@ -10,6 +16,7 @@ def kitaev_ft_Hamiltonian_FT_OBC(kx, Jx, Jy, Jz, a0, N):
     for y in range(N):
         for y1 in range(N):
             H_ft = np.zeros((2,2), dtype=complex)
+            # Fourier transform from ky to real-space y,y1 matrix elements
             for ky in ky_vals:
                 dx_k = Jy * np.sin(kx * a0) + Jz * np.sin(ky * a0)
                 dy_k = Jx + Jy * np.cos(kx * a0) + Jz * np.cos(ky * a0)
@@ -19,12 +26,12 @@ def kitaev_ft_Hamiltonian_FT_OBC(kx, Jx, Jy, Jz, a0, N):
             bigH[2*y:2*y+2, 2*y1:2*y1+2] = H_ft
     bigH[0:2, -2:] = 0
     bigH[-2:, 0:2] = 0
-    #bigH = np.where(np.abs(bigH.real) < threshold, 0, bigH.real) + 1j * np.where(np.abs(bigH.imag) < threshold, 0, bigH.imag)
+    #bigH = np.where(np.abs(bigH.real) < threshold, 0, bigH.real) + 1j * np.where(np.abs(bigH.imag) < threshold, 0, bigH.imag) # not needed
     return bigH
 delta = 0 # testing 
-N = 100
+N = 100 # discritisation number 
 kx_vals = np.linspace(0, 2*np.pi, 100)
-J0 = 0.225
+J0 = 0.45
 a0 = 1.0
 T = 2 * np.pi
 t_step = T / 6
@@ -47,14 +54,14 @@ for i, kx_val in enumerate(kx_vals):
         U_total = U @ U_total
     eigvals[i, :] = np.linalg.eigvals(U_total)
 
-epsilon_T = -np.angle(eigvals)  # Shape (len(kx), 2*N)
+epsilon_T = -np.angle(eigvals)  
 
 bands_sorted = np.sort(epsilon_T, axis=1)  # Sort at each kx (2*N bands for N=20)
 
 
 for band in range(bands_sorted.shape[1]):
     plt.plot(kx_vals, bands_sorted[:, band], color='limegreen', lw=0.7, alpha=0.8)
-plt.title('Floquet Quasi-energy Bands Driven Kitaev Model (perturbed, delta=0.66, N=40)')
+plt.title('Floquet Quasi-energy Bands Driven Kitaev Model (perturbed, delta=0.66, N=100)') # Plot name can be changed depending on what you are doing
 plt.xlabel(r'$k_x a_0$')
 plt.ylabel(r'$\varepsilon T$')
 plt.xlim([0, 2*np.pi])
